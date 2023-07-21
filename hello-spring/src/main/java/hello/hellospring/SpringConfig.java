0c06146948bb1c4505c74aff7daa2943195a6aa4
realisshomyang <realisshoman@kakao.com>
2023-07-22 04:02:32 +0900

    2023-07-22 Spring 입문 강의 완료

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,30 +1,53 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
+    /*
+    등록한게 없지 않나? 인터페이스에 대한 구현체를 스프링 데이터 JPA가 해준다
+     */
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    /*
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+    */
+
+    /*
+        private DataSource dataSource;
+        @Autowired
+        public SpringConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }*/
+
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository(){
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+    //@Bean
+    //public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcMemberRepository();
+      //  return new JpaMemberRepository(em);
+    //}
 
 }
--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+/*
+jdbc 쓰려면 엔티티 매핑을 해야함
+jpa 표준 인터페이스 구현은 업체마다 하는건데 하이버네티스꺼 쓴다.
+jpa Orm object relational data table mapping
+ */
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
 
     public long getId() {
